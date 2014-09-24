90cdd1fec444b2ede31ae7b4733b8be8f320fc7f
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2014-09-24 10:36:27 +0900

    typo in .gitignore
    /resources/data/create.sql が正しい。

---
+++ resources/data/create.sql
@@ -0,0 +1,20 @@
+-- enq4 schema
+
+drop table if exists enq4;
+create table enq4 (
+       id    integer primary key,
+       name  varchar(255) not null,
+       subject varchar(255) not null,
+       q1      varchar(255) not null,
+       q2      varchar(255) not null,
+       q3      varchar(255) not null,
+       q4      varchar(255) not null,
+       original varchar(255) not null, -- url for downloading original
+       upload   varchar(255) not null, -- url for uploaded file
+       timestamp datetime default null
+);
+
+insert into enq4 (name, subject, q1, q2, q3, q4, original, upload) values
+       ('kimura', 'clojure', 'q1', 'q2', 'q3', 'q4', 'original', 'update');
+insert into enq4 (name, subject, q1, q2, q3, q4, original, upload) values
+       ('hiroshi', 'ruby', 'q1', 'q2', 'q3', 'q4','down', 'up');
